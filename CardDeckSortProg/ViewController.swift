f6fa54e3e5eb13fa3676190603d0499094f2ee87
Rechimon, Andrés <andres.r1995@hotmail.com>
2023-11-27 16:24:45 -0300

    Initial Commit

---
+++ CardDeckSortProg/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CardDeckSortProg
+//
+//  Created by Administrador on 27/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
